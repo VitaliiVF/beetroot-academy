851b6c377c17522b45f68d8e16609f827de5079d
VitaliiVF <deferend794@gmail.com>
2022-07-13 22:00:17 +0300

    homework functions

--- Homework/HW L8/task1.py
+++ Homework/HW L8/task1.py
@@ -1,4 +1,4 @@
 def favorite_movie(movie: str) -> str:
     return f"My favorite movie is named {movie}"
 
-print(favorite_movie("Eternal Sunshine of the Spotless Mind"))
+favorite_movie("Eternal Sunshine of the Spotless Mind")
